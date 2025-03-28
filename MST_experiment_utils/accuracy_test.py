47e2fa8f132414f377fe97f89ffbe3b6a38aac82
Tianyi Zhu <tzhu@caltech.edu>
2025-03-27 17:44:06 -0700

    fix cleanup bug

--- MST_experiment_utils/accuracy_test.py
+++ MST_experiment_utils/accuracy_test.py
@@ -1,10 +1,11 @@
-from .run_test import run_single_test
+from .run_test import run_single_test, cuda_cleanup
 import numpy as np
 
 def run_needle_test(needleGenerator, model_generator, runner_generator, context_lens, placements, max_context = None, print_answer = True):
     results = np.zeros((len(placements), len(context_lens)))
     test_runned = 0
     total_tests = len(context_lens)* len(placements)
+    cuda_cleanup(True)
     for i, context_len in enumerate(context_lens):
         for j, placement in enumerate(placements):
             test_runned += 1
@@ -14,6 +15,7 @@ def run_needle_test(needleGenerator, model_generator, runner_generator, context_
             needle_generator = needleGenerator(context_len, placement, max_context)
             stats, answer = run_single_test(needle_generator.generate, model_generator, runner_generator)
             results[j, i] = needle_generator.evaluate(answer)
+            cuda_cleanup(True)
             
             if print_answer:
                 print('Response: ', answer)
