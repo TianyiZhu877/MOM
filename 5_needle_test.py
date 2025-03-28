c64ed491525101bd7bf1d921e66cc96050f874ff
Tianyi Zhu <tzhu@caltech.edu>
2025-03-27 17:22:37 -0700

    implement needle test

---
+++ 5_needle_test.py
@@ -0,0 +1,39 @@
+import transformers
+import torch
+from transformers.cache_utils import Cache, DynamicCache, StaticCache, OffloadedCache, OffloadedStaticCache
+from MST_experiment_utils.MSI import minisequence_inference
+from MST_experiment_utils import *
+import importlib
+
+
+model_ckpt = "gradientai/Llama-3-8B-Instruct-Gradient-1048k"
+# model_ckpt = "meta-llama/Llama-3.2-3B-Instruct"
+
+# quantization_config = transformers.BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_compute_dtype="float16"  # Computation done in float16
+# )
+quantization_config = None
+
+
+
+# context_lens = [6000, 12000, 18000, 24000]
+context_lens = [5000, 10000, 15000, 20000, 25000, 30000, 35000]
+placements = [0, 0.25, 0.5, 0.75, 1]
+
+name = 'MOM'
+model_generator = lambda: general_model(model_ckpt, MST=minisequence_inference, quantization = quantization_config)
+runner_generator = lambda:  decodeOnlyOffload(max_new_tokens=100, stop_by_eos = True)
+# max_context = 24000
+max_context = 350000
+
+
+# name = 'standard'
+# model_generator = lambda: general_model(model_ckpt, MST=False, quantization = quantization_config)
+# runner_generator = lambda: regularRecursive(stop_by_eos = True, max_new_tokens=100)
+# max_context = 12000
+# max_context = 145000
+
+results = run_needle_test(needleInBook, model_generator, runner_generator, context_lens, placements, max_context)
+print(results)
+np.save("outputs/5_results_"+name+".npy", results)
---
+++ MST_experiment_utils/accuracy_test.py
@@ -0,0 +1,23 @@
+from .run_test import run_single_test
+import numpy as np
+
+def run_needle_test(needleGenerator, model_generator, runner_generator, context_lens, placements, max_context = None, print_answer = True):
+    results = np.zeros((len(placements), len(context_lens)))
+    test_runned = 0
+    total_tests = len(context_lens)* len(placements)
+    for i, context_len in enumerate(context_lens):
+        for j, placement in enumerate(placements):
+            test_runned += 1
+            print('********************************')
+            print(f'Test {test_runned}/{total_tests}: {context_len} length placing at {placement}')
+            # print()
+            needle_generator = needleGenerator(context_len, placement, max_context)
+            stats, answer = run_single_test(needle_generator.generate, model_generator, runner_generator)
+            results[j, i] = needle_generator.evaluate(answer)
+            
+            if print_answer:
+                print('Response: ', answer)
+
+            del needle_generator
+    
+    return results
